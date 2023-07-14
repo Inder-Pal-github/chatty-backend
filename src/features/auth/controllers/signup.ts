3243fdbc7e5e5157ae5174a10a2a3a7958f75bf4
Inder Pal <inder39811@gmail.com>
2023-07-14 08:37:40 +0530

    Updated: addeding controllers

---
+++ src/features/auth/controllers/signup.ts
@@ -0,0 +1,10 @@
+import {ObjectId}  from 'mongodb';
+import {Request,Response} from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+
+
+export class Signup {
+  public async create(req:Request,res:Response):Promise<void> {
+
+  };
+}
